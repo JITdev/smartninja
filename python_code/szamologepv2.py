c861bb144a7dd852b4580294c8d25f35bcdbd100
Kainrath Roland <justintimedev@gmail.com>
2020-03-26 09:45:11 +0100

    Minor fixes

--- python_code/szamologepv2.py
+++ python_code/szamologepv2.py
@@ -1,15 +1,13 @@
 
 from functions_11 import calculate
 
-num_1 = int(input('Please enter a number:'))
-num_2 = int(input('Please enter a number:'))
+num_1 = int(input('Please enter a number: '))
+num_2 = int(input('Please enter a number: '))
 operand = input('Please select an operation: (+ - * /): ')
 
 print('-' * 20)
 
 result = calculate(num_1, num_2, operand)
 
-print(f'Result: {result}')
-    
+print(f'Result: {result}')    
 print('-' * 20)
-print('End!')
