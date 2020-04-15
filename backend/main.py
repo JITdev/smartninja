ea64b7596c80316f9fb503ae4b9ee8e408d331e6
Kainrath Roland <justintimedev@gmail.com>
2020-04-15 19:15:11 +0200

    POST in flask & set cookies

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__, static_folder='./static', template_folder='./templates')
 
@@ -15,10 +15,37 @@ def index():
     return render_template("index.html", some_text=text, current_year=year, cities=cities)
 
 
-@app.route("/about-me")
+@app.route("/about-me", methods=['GET', 'POST'])
 def about():
-    return render_template("about.html")
+    if request.method == "GET":
+        user_name = request.cookies.get("user_name")
+        return render_template("about.html", name=user_name)
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
 
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+
+        return response
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 if __name__ == '__main__':
     app.run()
