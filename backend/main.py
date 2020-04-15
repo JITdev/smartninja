d7d2db5c2427a790c43287a311b88c6fa7070b6f
Kainrath Roland <justintimedev@gmail.com>
2020-04-15 17:55:57 +0200

    Templates

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='./static', template_folder='./templates')
@@ -5,7 +7,18 @@ app = Flask(__name__, static_folder='./static', template_folder='./templates')
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    text = "Message from the handler."
+    year = datetime.datetime.now().year
+
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=text, current_year=year, cities=cities)
+
+
+@app.route("/about-me")
+def about():
+    return render_template("about.html")
+
 
 if __name__ == '__main__':
     app.run()
