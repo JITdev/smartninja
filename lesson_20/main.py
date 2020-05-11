91311df018d96026286998220100b39feae8a3a9
Kainrath Roland <justintimedev@gmail.com>
2020-05-11 17:57:45 +0200

    Lession 20

--- lesson_20/main.py
+++ lesson_20/main.py
@@ -3,7 +3,7 @@ import random
 import uuid
 
 from flask import Flask, render_template, request, make_response, redirect, url_for
-from models import User, db
+from .models import User, db
 
 app = Flask(__name__)
 db.create_all()  # create (new) tables in the database
