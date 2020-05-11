48a5da01052a7521a0a3ec3b68d91daa59984084
Kainrath Roland <justintimedev@gmail.com>
2020-05-11 22:01:53 +0200

    Icon added

--- weather/main.py
+++ weather/main.py
@@ -8,20 +8,20 @@ app = Flask(__name__, static_folder='./static', template_folder='./templates')
 
 @app.route("/")
 def index():
-    query = request.args.get('city', 'London,UK')
+    params = {
+        'q': request.args.get('city', 'London,UK'),
+        'units': "metric",  # use "imperial" for Fahrenheit
+        'appid': "7c5fe4b501cba91a84cca4170f89ec0f",
+    }
+    url = f'https://api.openweathermap.org/data/2.5/weather'
 
-    unit = "metric"  # use "imperial" for Fahrenheit
-    api_key = "7c5fe4b501cba91a84cca4170f89ec0f"
+    data = requests.get(url=url, params=params)  # GET request to the OpenWeatherMap API
 
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={query}&units={unit}&appid={api_key}"
-
-    data = requests.get(url=url)  # GET request to the OpenWeatherMap API
-
-    return render_template("index.html", data=data.json())
+    return render_template('index.html', data=data.json())
 
 
 if __name__ == '__main__':
-    d = {'a': 'b'}
-    json.dumps(d)
-    json.loads('{"a": "b"}')
+    # d = {'a': 'b'}
+    # json.dumps(d)
+    # json.loads('{"a": "b"}')
     app.run(port=5001)
